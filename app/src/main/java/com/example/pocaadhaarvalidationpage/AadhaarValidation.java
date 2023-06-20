a1aea0fa0a9b938690a5f879a74dad6a2a8d8d49
Rajasekar <rs20jn1998s@gmail.com>
2023-06-20 19:18:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pocaadhaarvalidationpage/AadhaarValidation.java
@@ -0,0 +1,14 @@
+package com.example.pocaadhaarvalidationpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AadhaarValidation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aadhaar_validation);
+    }
+}
